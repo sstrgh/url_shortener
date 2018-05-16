f83be8968bfb004c6068cfbec15981e680cc1a6e
Satrughan <sunnydeveloperacc@gmail.com>
2018-05-16 17:37:40 +0800

    Added validation tests for base_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,2 +1,8 @@
 class ShortUrl < ApplicationRecord
+
+  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\Z/ix
+
+  validates :base_url, format: { with: URL_REGEXP, message: 'Invalid URL' }
+  validates :base_url, presence: true, on: :create
+
 end
--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ShortUrl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "validates presence of base_url" do
+    expect(ShortUrl.new()).not_to be_valid
+    expect(ShortUrl.new(base_url: "https://www.google/")).not_to be_valid
+    expect(ShortUrl.new(base_url: "https://www.google.com/")).to be_valid
+  end
+
+
+  
 end
