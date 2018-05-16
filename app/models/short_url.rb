1b2c657aa683d7cab4b081644368d7e3a4bb88e2
Satrughan <sunnydeveloperacc@gmail.com>
2018-05-16 17:44:15 +0800

    Added url sanitization functions

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -5,4 +5,10 @@ class ShortUrl < ApplicationRecord
   validates :base_url, format: { with: URL_REGEXP, message: 'Invalid URL' }
   validates :base_url, presence: true, on: :create
 
+  before_create :sanitize_url
+
+  def sanitize_url
+    self.base_url = self.base_url.downcase.strip
+  end
+
 end
--- spec/models/short_url_spec.rb
+++ spec/models/short_url_spec.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 RSpec.describe ShortUrl, type: :model do
 
   it "validates presence of base_url" do
-    expect(ShortUrl.new()).not_to be_valid
-    expect(ShortUrl.new(base_url: "https://www.google/")).not_to be_valid
-    expect(ShortUrl.new(base_url: "https://www.google.com/")).to be_valid
+    expect(ShortUrl.create()).not_to be_valid
+    expect(ShortUrl.create(base_url: "https://www.google/")).not_to be_valid
+    expect(ShortUrl.create(base_url: "https://www.google.com/")).to be_valid
   end
 
+ it "sanitizes base_url on create" do
+   short_url = ShortUrl.create(base_url: "https://www.Google.com/   ")
+   expect(short_url.base_url).to eq "https://www.google.com/"
+ end
 
-  
 end
